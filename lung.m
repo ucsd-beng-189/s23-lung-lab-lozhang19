4a9832e4cfb17f73b92b170cd08ced727e7a03fd
Longyu Zhang <lozhang@ucsd.edu>
2023-04-19 04:26:14 -0700

    task3

--- lung.m
+++ lung.m
@@ -1,7 +1,15 @@
 %filename: lung.m (main program)
-clear all
-clf
-global Pstar cstar n maxcount M Q camax RT cI;
+function [PI, PAbar, Pabar, Pv] = lung(beta)
+    global Pstar cstar n maxcount M Q camax RT cI;
 setup_lung
+
+a1=-log(rand(n,1));
+a2=-log(rand(n,1));
+av=(a1+a2)/2;        
+VA=VAbar*(a1*beta+av*(1-beta));
+Q = Qbar*(a2*beta+av*(1-beta));
+r=VA./Q;
+
 cvsolve
 outchecklung
+end
---
+++ task3.m
@@ -0,0 +1,32 @@
+% Task3
+clear all;
+clf;
+global Pstar cstar n maxcount M Q camax RT cI;
+
+setup_lung
+betas = 0.1:0.1:1;
+PI_values = zeros(size(betas));
+Pabar_values = zeros(size(betas));
+PAbar_values = zeros(size(betas));
+Pv_values = zeros(size(betas));
+
+for i=1:length(betas)
+    beta=betas(i);
+    [PI, PAbar, Pabar, Pv] = lung(beta);
+    PI_values = PI;
+    Pabar_values = Pabar;
+    PAbar_values = PAbar;
+    Pv_values = Pv;
+end
+
+figure;
+hold on;
+plot(betas, PI_values, 'DisplayName', 'Inspired Oxygen Partial Pressure (PiO2)');
+plot(betas, PAbar_values, 'DisplayName', 'Mean Alveolar Oxygen Partial Pressure (PAO2)');
+plot(betas, Pabar_values, 'DisplayName', 'Mean Arterial Oxygen Partial Pressure (PaO2)');
+plot(betas, Pv_values, 'DisplayName', 'Venous Oxygen Partial Pressure (PvO2)');
+xlabel('Beta');
+ylabel('Partial Pressure (mmHg)');
+legend;
+title('Partial Pressures as functions of Beta');
+hold off;
