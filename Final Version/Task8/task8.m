103a2934ef515dbe21ddd7b9d20af918e40ce615
Longyu Zhang <lozhang@ucsd.edu>
2023-04-23 07:10:03 -0700

    Task8 Final Version Update 3.0

--- Final Version/Task8/task8.m
+++ Final Version/Task8/task8.m
@@ -3,45 +3,70 @@ clear all;
 clf;
 global Pstar cstar n maxcount M Q camax RT cI;
 
-altitudes = [0:100:2000];
-
-T0=288.15; %sea level standard temperature(K)
-P0=101325;%sea level standard atmospheric pressure
-M_air=0.029;
-R=8.314;%specific gas constant for dry air
-g0=9.8;%standard acceleration of gravity(m/s)
+altitudes = [0:1:1000];
 
 for i=1:length(altitudes)
     altitude = altitudes(i);
-    Patm = P0*exp(-g0*M_air*altitude/(R*T0));
-    Patm_mmHg=Patm*0.0075;
-    PH2O=47;
-    FiO2=0.2095;
-    PI=(Patm_mmHg-PH2O)*FiO2;
-    if PI < 0
-    PI = 0.1;
-    end
-   
-    RT=760*22.4*(T0/273.15);
-    cI=PI/RT;
-    cref=0.2/(22.4*(310/273));
-    cstar=cref;
+    RT=760*22.4*(310/273);
+    cref = (0.2 / (22.4 + altitude)) * (1 / (310/273));
+    cI=cref; 
     setup_lung
+    try
     cvsolve
     outchecklung
-    [~, PAbar, Pabar, Pv] = lung(0.5);
+     
+    [~, PAbar, Pabar, Pv] = lung();
+    [~, cAbar, cabar, cv] = clung();
     Pabar_values(i) = Pabar;
     PAbar_values(i) = PAbar;
     Pv_values(i) = Pv;
+    ca_values(i) = cabar;
+    cA_values(i) = cAbar;
+    cv_values(i) = cv;
+    
+catch ME
+    if strcmp(ME.message,'M is too large')
+        fprintf('At altitude %d meters, it becomes impossible to sustain the normal resting rate of oxygen consumption (without breathing harder or increasing the cardiac output).\n', altitude);
+        Pabar_values( i) = NaN;
+        PAbar_values( i) = NaN;
+        Pv_values( i) = NaN;
+        ca_values(i) = NaN;
+        cA_values(i) = NaN;
+        cv_values(i) = NaN;
+        break;
+            else
+                rethrow(ME)
+    end
+    end
 end
+
+valid_indices = ~isnan(PAbar_values);
+altitudes = altitudes(valid_indices);
+PAbar_values = PAbar_values(valid_indices);
+Pabar_values = Pabar_values(valid_indices);
+Pv_values = Pv_values(valid_indices);
+ca_values = ca_values(valid_indices);
+cA_values = cA_values(valid_indices);
+cv_values = cv_values(valid_indices);
 
 figure;
 hold on;
-plot(altitudes, PAbar_values, 'DisplayName', ['Pabar,beta=' num2str(beta)]);
-plot(altitudes, Pabar_values, 'DisplayName', ['Pabar,beta=' num2str(beta)]);
-plot(altitudes, Pv_values, 'DisplayName', ['Pabar,beta=' num2str(beta)]);
+plot(altitudes, PAbar_values, 'DisplayName', sprintf('PAbar, beta=%.2f', beta));
+plot(altitudes, Pabar_values, 'DisplayName',sprintf('Pabar, beta=%.2f', beta));
+plot(altitudes, Pv_values, 'DisplayName', sprintf('Pv, beta=%.2f', beta));
 xlabel('Altitude(m)');
 ylabel('Partial Pressure (mmHg)');
 legend;
-title('Partial Pressures as functions of Altitude and Beta');
+title('Partial Pressures as functions of Altitude');
+hold off;
+
+figure;
+hold on;
+plot(altitudes, cA_values, 'DisplayName', sprintf('cAbar, beta=%.2f', beta));
+plot(altitudes, ca_values, 'DisplayName', sprintf('cabar, beta=%.2f', beta));
+plot(altitudes, cv_values, 'DisplayName', sprintf('cv, beta=%.2f', beta));
+xlabel('Altitude(m)');
+ylabel('Oxygen concentraion');
+legend;
+title('Oxygen concentraion as functions of Altitude');
 hold off;
