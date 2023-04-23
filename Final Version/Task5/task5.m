5661e7168dd083100db55bec667a391a3d8ac6e3
Longyu Zhang <lozhang@ucsd.edu>
2023-04-22 17:50:59 -0700

    Task5 Final Version update

--- Final Version/Task5/task5.m
+++ Final Version/Task5/task5.m
@@ -3,7 +3,7 @@ clear all;
 clf;
 global Pstar cstar n maxcount M Q camax RT cI;
 
-cI_values = [0.1:0.01:1];
+cI_values = [0.004:0.0001:0.01];
 
 for i=1:length(cI_values)
     cI = cI_values(i);
@@ -11,29 +11,34 @@ for i=1:length(cI_values)
     setup_lung
     cvsolve
     outchecklung
-    [~, PAbar, Pabar, Pv] = lung(0.5);
+    [~, PAbar, Pabar, Pv] = lung();
+    [~, cAbar, cabar, cv] = clung();
     Pabar_values(i) = Pabar;
     PAbar_values(i) = PAbar;
     Pv_values(i) = Pv;
+    ca_values(i) = cabar;
+    cA_values(i) = cAbar;
+    cv_values(i) = cv;
 end
 
 figure;
-subplot(3,1,1);
+hold on;
 plot(cI_values, PAbar_values, 'DisplayName', 'PAbar');
-xlabel('cI');
-ylabel('Partial Pressure (mmHg)');
-legend;
-title('Partial Pressures as functions of cI');
-subplot(3,1,2);
 plot(cI_values, Pabar_values, 'DisplayName', 'Pabar');
-xlabel('cI');
-ylabel('Partial Pressure (mmHg)');
-legend;
-title('Partial Pressures as functions of cI');
-subplot(3,1,3);
 plot(cI_values, Pv_values, 'DisplayName', 'Pv');
 xlabel('cI');
 ylabel('Partial Pressure (mmHg)');
 legend;
 title('Partial Pressures as functions of cI');
+hold off;
+
+figure;
+hold on;
+plot(cI_values, cA_values, 'DisplayName', 'cAbar');
+plot(cI_values, ca_values, 'DisplayName', 'cabar');
+plot(cI_values, cv_values, 'DisplayName', 'cv');
+xlabel('cI');
+ylabel('Oxygen concentraion');
+legend;
+title('Oxygen concentraion as functions of cI');
 hold off;
